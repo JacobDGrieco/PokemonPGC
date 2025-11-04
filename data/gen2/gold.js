e04c582c86843d2c4bc46a08bbc5b43da199f711
JacobDGrieco <jacobdgrieco@gmail.com>
2025-11-04 14:53:21 -0500

    added setup for sections

--- data/gen2/gold.js
+++ data/gen2/gold.js
@@ -1,24 +1,38 @@
 PPGC.register({
-    /* Optional dex entries for that game */
+    /* Optional starter sections/tasks (picked up on first open) */
+    sections: {
+        gold: [
+            { id: "gold-story", title: "Main Story" },
+            { id: "gold-sidequests", title: "Side Quests" },
+            { id: "gold-catching", title: "Gotta Catch 'Em All" },
+            { id: "gold-legendaries", title: "Legendaries" },
+            { id: "gold-upgrades", title: "Upgrades" },
+            { id: "gold-collectables", title: "Collectables" },
+            { id: "gold-thms", title: "TMs/HMs" },
+        ]
+    },
+
+    tasks: {
+        "gold-story": [
+            { id: "gold-story-1", text: "Complete the Game", done: false },
+        ],
+        "gold-sidequests": [
+        ],
+        "gold-catching": [
+        ],
+        "gold-legendaries": [
+        ],
+        "gold-upgrades": [
+        ],
+        "gold-collectables": [
+        ],
+        "gold-thms": [
+        ],
+    },
+
     dex: {
         gold: [
             { id: 152, name: "Chikorita", img: "imgs/pokemon_home/normal/normal/152.png" },
         ]
     },
-
-    /* Optional starter sections/tasks (picked up on first open) */
-    sections: {
-        gold: [
-            { id: "gold-story", title: "Main Story" },
-            // { id: "blue-catching", title: "Gotta Catch 'Em All" },
-            // { id: "blue-legendaries", title: "Legendaries" },
-            // { id: "blue-collectables", title: "Collectables" },
-            // { id: "blue-thms", title: "TMs/HMs" },
-        ]
-    },
-    tasks: {
-        "gold-story": [
-            { id: "gold-story-1", text: "Complete the Game", done: false },
-        ]
-    }
 });
