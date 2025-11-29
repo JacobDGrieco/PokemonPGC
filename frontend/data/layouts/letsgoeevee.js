bfc971235135777327c2732f54eddac77823eaff
JacobDGrieco <jacobdgrieco@gmail.com>
2025-11-29 07:32:30 -0500

    added tms, hms, and trs to all games

--- frontend/data/layouts/letsgoeevee.js
+++ frontend/data/layouts/letsgoeevee.js
@@ -6,6 +6,21 @@ PPGC.register({
 					["letsgoeevee-catching-1"],
 					["letsgoeevee-catching-1-a"],
 				],
+				"letsgoeevee-thms": [
+					["letsgoeevee-thms-1"],
+					["letsgoeevee-thms-1-a", "letsgoeevee-thms-1-b", "letsgoeevee-thms-1-c", "letsgoeevee-thms-1-d", "letsgoeevee-thms-1-e"],
+					["letsgoeevee-thms-2"],
+					["letsgoeevee-thms-2-01", "letsgoeevee-thms-2-11", "letsgoeevee-thms-2-21", "letsgoeevee-thms-2-31", "letsgoeevee-thms-2-41", "letsgoeevee-thms-2-51"],
+					["letsgoeevee-thms-2-02", "letsgoeevee-thms-2-12", "letsgoeevee-thms-2-22", "letsgoeevee-thms-2-32", "letsgoeevee-thms-2-42", "letsgoeevee-thms-2-52"],
+					["letsgoeevee-thms-2-03", "letsgoeevee-thms-2-13", "letsgoeevee-thms-2-23", "letsgoeevee-thms-2-33", "letsgoeevee-thms-2-43", "letsgoeevee-thms-2-53"],
+					["letsgoeevee-thms-2-04", "letsgoeevee-thms-2-14", "letsgoeevee-thms-2-24", "letsgoeevee-thms-2-34", "letsgoeevee-thms-2-44", "letsgoeevee-thms-2-54"],
+					["letsgoeevee-thms-2-05", "letsgoeevee-thms-2-15", "letsgoeevee-thms-2-25", "letsgoeevee-thms-2-35", "letsgoeevee-thms-2-45", "letsgoeevee-thms-2-55"],
+					["letsgoeevee-thms-2-06", "letsgoeevee-thms-2-16", "letsgoeevee-thms-2-26", "letsgoeevee-thms-2-36", "letsgoeevee-thms-2-46", "letsgoeevee-thms-2-56"],
+					["letsgoeevee-thms-2-07", "letsgoeevee-thms-2-17", "letsgoeevee-thms-2-27", "letsgoeevee-thms-2-37", "letsgoeevee-thms-2-47", "letsgoeevee-thms-2-57"],
+					["letsgoeevee-thms-2-08", "letsgoeevee-thms-2-18", "letsgoeevee-thms-2-28", "letsgoeevee-thms-2-38", "letsgoeevee-thms-2-48", "letsgoeevee-thms-2-58"],
+					["letsgoeevee-thms-2-09", "letsgoeevee-thms-2-19", "letsgoeevee-thms-2-29", "letsgoeevee-thms-2-39", "letsgoeevee-thms-2-49", "letsgoeevee-thms-2-59"],
+					["letsgoeevee-thms-2-10", "letsgoeevee-thms-2-20", "letsgoeevee-thms-2-30", "letsgoeevee-thms-2-40", "letsgoeevee-thms-2-50", "letsgoeevee-thms-2-60"]
+				]
 			},
 		},
 		compact: {
--- frontend/data/layouts/letsgopikachu.js
+++ frontend/data/layouts/letsgopikachu.js
@@ -6,6 +6,21 @@ PPGC.register({
 					["letsgopikachu-catching-1"],
 					["letsgopikachu-catching-1-a"],
 				],
+				"letsgopikachu-thms": [
+					["letsgopikachu-thms-1"],
+					["letsgopikachu-thms-1-a", "letsgopikachu-thms-1-b", "letsgopikachu-thms-1-c", "letsgopikachu-thms-1-d", "letsgopikachu-thms-1-e"],
+					["letsgopikachu-thms-2"],
+					["letsgopikachu-thms-2-01", "letsgopikachu-thms-2-11", "letsgopikachu-thms-2-21", "letsgopikachu-thms-2-31", "letsgopikachu-thms-2-41", "letsgopikachu-thms-2-51"],
+					["letsgopikachu-thms-2-02", "letsgopikachu-thms-2-12", "letsgopikachu-thms-2-22", "letsgopikachu-thms-2-32", "letsgopikachu-thms-2-42", "letsgopikachu-thms-2-52"],
+					["letsgopikachu-thms-2-03", "letsgopikachu-thms-2-13", "letsgopikachu-thms-2-23", "letsgopikachu-thms-2-33", "letsgopikachu-thms-2-43", "letsgopikachu-thms-2-53"],
+					["letsgopikachu-thms-2-04", "letsgopikachu-thms-2-14", "letsgopikachu-thms-2-24", "letsgopikachu-thms-2-34", "letsgopikachu-thms-2-44", "letsgopikachu-thms-2-54"],
+					["letsgopikachu-thms-2-05", "letsgopikachu-thms-2-15", "letsgopikachu-thms-2-25", "letsgopikachu-thms-2-35", "letsgopikachu-thms-2-45", "letsgopikachu-thms-2-55"],
+					["letsgopikachu-thms-2-06", "letsgopikachu-thms-2-16", "letsgopikachu-thms-2-26", "letsgopikachu-thms-2-36", "letsgopikachu-thms-2-46", "letsgopikachu-thms-2-56"],
+					["letsgopikachu-thms-2-07", "letsgopikachu-thms-2-17", "letsgopikachu-thms-2-27", "letsgopikachu-thms-2-37", "letsgopikachu-thms-2-47", "letsgopikachu-thms-2-57"],
+					["letsgopikachu-thms-2-08", "letsgopikachu-thms-2-18", "letsgopikachu-thms-2-28", "letsgopikachu-thms-2-38", "letsgopikachu-thms-2-48", "letsgopikachu-thms-2-58"],
+					["letsgopikachu-thms-2-09", "letsgopikachu-thms-2-19", "letsgopikachu-thms-2-29", "letsgopikachu-thms-2-39", "letsgopikachu-thms-2-49", "letsgopikachu-thms-2-59"],
+					["letsgopikachu-thms-2-10", "letsgopikachu-thms-2-20", "letsgopikachu-thms-2-30", "letsgopikachu-thms-2-40", "letsgopikachu-thms-2-50", "letsgopikachu-thms-2-60"]
+				]
 			},
 		},
 		compact: {
