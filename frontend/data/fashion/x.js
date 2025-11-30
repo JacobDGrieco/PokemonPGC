29d9494709f363402590201368b5a9441d0f619d
JacobDGrieco <jacobdgrieco@gmail.com>
2025-11-29 22:27:22 -0500

    started Pokemon X fashion data.

---
+++ frontend/data/fashion/x.js
@@ -0,0 +1,70 @@
+window.DATA.fashion = window.DATA.fashion || {};
+window.DATA.fashion["legendsza"] = {
+	categories: [
+		{
+			id: "hats",
+			label: "Hats",
+			items: [
+				{
+					id: "camo-cap",
+					name: "Camo Cap",
+					img: "",
+					forms: [
+						{ id: "camo-cap-1", name: "Aqua", img: "" },
+						{ id: "camo-cap-2", name: "Olive", img: "" },
+					],
+				},
+				{
+					id: "knit-cap",
+					name: "Knit Cap",
+					img: "",
+					forms: [
+						{ id: "camo-cap-1", name: "Black", img: "" },
+						{ id: "camo-cap-2", name: "Orange", img: "" },
+						{ id: "camo-cap-3", name: "Purple", img: "" },
+						{ id: "camo-cap-4", name: "White", img: "" },
+					],
+				},
+				{
+					id: "outdoors-cap",
+					name: "Outdoors Cap",
+					img: "",
+					forms: [
+						{ id: "outdoors-cap-1", name: "Beige", img: "" },
+						{ id: "outdoors-cap-2", name: "Black", img: "" },
+						{ id: "outdoors-cap-3", name: "Olive", img: "" },
+						{ id: "outdoors-cap-4", name: "Red", img: "" },
+					],
+				},
+				{
+					id: "fedora-cap",
+					name: "Fedora Cap",
+					img: "",
+					forms: [
+						{ id: "fedora-cap-2", name: "Black", img: "" },
+						{ id: "fedora-cap-2", name: "Gray", img: "" },
+						{ id: "fedora-cap-3", name: "Red", img: "" },
+						{ id: "fedora-cap-1", name: "Checkered Black", img: "" },
+					],
+				},
+				{
+					id: "logo-cap",
+					name: "Logo Cap",
+					img: "",
+					forms: [
+						{ id: "logo-cap-1", name: "Black", img: "" },
+						{ id: "logo-cap-2", name: "Blue", img: "" },
+						{ id: "logo-cap-3", name: "Green", img: "" },
+						{ id: "logo-cap-3", name: "Red", img: "" },
+					],
+				},
+				{
+					id: "bamboo-sprig-hat",
+					name: "Bamboo Sprig Hat",
+					img: "",
+				},
+			]
+		}
+	],
+};
+
