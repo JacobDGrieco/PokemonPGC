63face6d7fe81b02e74c726d582f0af999fe4e64
JacobDGrieco <jacobdgrieco@gmail.com>
2025-12-07 07:20:59 -0500

    fixed Emerald to new format

--- frontend/data/layouts/emerald.js
+++ frontend/data/layouts/emerald.js
@@ -1,63 +1,86 @@
+// Game
+const game = "emerald";
+
+// Categories
+const catching = game + "-catching";
+const story = game + "-story";
+const activities = game + "-activities";
+const battle = game + "-battle";
+const upgrades = game + "-upgrades";
+const collectables = game + "-collectables";
+const thms = game + "-thms";
+const distributions = game + "-distributions";
+const extraCredit = game + "-extra-credit";
+
 PPGC.register({
 	layoutVariants: {
 		desktop: {
 			taskRows: {
-				"emerald-catching": [
-					["emerald-catching-1"],
-					["emerald-catching-1-a", "emerald-catching-1-b", "emerald-catching-1-c"],
-					["emerald-catching-1-d", "emerald-catching-1-e", "emerald-catching-1-f"],
-					["emerald-catching-1-g", "emerald-catching-1-h"],
-					["emerald-catching-2"],
-					["emerald-catching-2-a", "emerald-catching-2-b", "emerald-catching-2-c", "emerald-catching-2-d"],
-					["emerald-catching-3"],
-					["emerald-catching-3-a", "emerald-catching-3-b", "emerald-catching-3-c"],
+				[catching]: [
+					[catching + "-1"],
+					[catching + "-1-01", catching + "-1-02", catching + "-1-03"],
+					[catching + "-1-04", catching + "-1-05", catching + "-1-06"],
+					[catching + "-1-07", catching + "-1-08"],
+					[catching + "-1-09", catching + "-1-10"],
+					[catching + "-2"],
+					[catching + "-2-01", catching + "-2-02", catching + "-2-03", catching + "-2-04"],
+					[catching + "-3"],
+					[catching + "-3-01", catching + "-3-02", catching + "-3-03"],
 				],
-				"emerald-story": [
-					["emerald-story-1"],
+				[story]: [
+					[story + "-1"],
 				],
-				"emerald-activities": [
-					["emerald-activities-1"],
-					["emerald-activities-1-a", "emerald-activities-1-b", "emerald-activities-1-c"],
-					["emerald-activities-1-d", "emerald-activities-1-e"],
-					["emerald-activities-2"],
+				[activities]: [
+					[activities + "-1"],
+					[activities + "-1-01", activities + "-1-02", activities + "-1-03"],
+					[activities + "-1-04", activities + "-1-05"],
+					[activities + "-2"],
 				],
-				"emerald-battle": [
-					["emerald-battle-1"],
-					["emerald-battle-2"],
+				[battle]: [
+					[battle + "-1"],
+					[battle + "-2"],
+					[battle + "-2-01", battle + "-2-02", battle + "-2-03", battle + "-2-04"],
+					[battle + "-3"],
+					[battle + "-3-01", battle + "-3-02", battle + "-3-03"],
+					[battle + "-4"],
+					[battle + "-4-01", battle + "-4-02", battle + "-4-03", battle + "-4-04"],
+					[battle + "-4-05", battle + "-4-06", battle + "-4-07", spacer],
+					[battle + "-5"],
+					[battle + "-5-01", battle + "-5-02", battle + "-5-03", battle + "-5-04"],
+					[battle + "-5-05", battle + "-5-06", battle + "-5-07", spacer],
 				],
-				"emerald-upgrades": [
-					["emerald-upgrades-1"],
-					["emerald-upgrades-2"],
-					["emerald-upgrades-2-a", "emerald-upgrades-2-b", "emerald-upgrades-2-c", "emerald-upgrades-2-d"],
+				[upgrades]: [
+					[upgrades + "-1"],
+					[upgrades + "-2"],
+					[upgrades + "-2-01", upgrades + "-2-02", upgrades + "-2-03", upgrades + "-2-04"],
 				],
-				"emerald-collectables": [
-					["emerald-collectables-1"],
-					["emerald-collectables-1-a", "emerald-collectables-1-c", "emerald-collectables-1-e"],
-					["emerald-collectables-1-b", "emerald-collectables-1-d", "emerald-collectables-1-f"],
-					["emerald-collectables-2"],
-					["emerald-collectables-2-a", "emerald-collectables-2-b", "emerald-collectables-2-c"],
-					["emerald-collectables-3"],
-					["emerald-collectables-3-a", "emerald-collectables-3-b"],
-					["emerald-collectables-4"],
+				[collectables]: [
+					[collectables + "-1"],
+					[collectables + "-1-01", collectables + "-1-02", collectables + "-1-03"],
+					[collectables + "-1-04", collectables + "-1-05", collectables + "-1-06"],
+					[collectables + "-1-07", collectables + "-1-08", collectables + "-1-09"],
+					[collectables + "-2"],
+					[collectables + "-2-01", collectables + "-2-02"],
+					[collectables + "-3"],
 				],
-				"emerald-thms": [
-					["emerald-thms-1"],
-					["emerald-thms-1-a", "emerald-thms-1-c", "emerald-thms-1-e", "emerald-thms-1-g"],
-					["emerald-thms-1-b", "emerald-thms-1-d", "emerald-thms-1-f", "emerald-thms-1-h"],
-					["emerald-thms-2"],
-					["emerald-thms-2-a", "emerald-thms-2-k", "emerald-thms-2-u", "emerald-thms-2-ae", "emerald-thms-2-ao"],
-					["emerald-thms-2-b", "emerald-thms-2-l", "emerald-thms-2-v", "emerald-thms-2-af", "emerald-thms-2-ap"],
-					["emerald-thms-2-c", "emerald-thms-2-m", "emerald-thms-2-w", "emerald-thms-2-ag", "emerald-thms-2-aq"],
-					["emerald-thms-2-d", "emerald-thms-2-n", "emerald-thms-2-x", "emerald-thms-2-ah", "emerald-thms-2-ar"],
-					["emerald-thms-2-e", "emerald-thms-2-o", "emerald-thms-2-y", "emerald-thms-2-ai", "emerald-thms-2-as"],
-					["emerald-thms-2-f", "emerald-thms-2-p", "emerald-thms-2-z", "emerald-thms-2-aj", "emerald-thms-2-at"],
-					["emerald-thms-2-g", "emerald-thms-2-q", "emerald-thms-2-aa", "emerald-thms-2-ak", "emerald-thms-2-au"],
-					["emerald-thms-2-h", "emerald-thms-2-r", "emerald-thms-2-ab", "emerald-thms-2-al", "emerald-thms-2-av"],
-					["emerald-thms-2-i", "emerald-thms-2-s", "emerald-thms-2-ac", "emerald-thms-2-am", "emerald-thms-2-aw"],
-					["emerald-thms-2-j", "emerald-thms-2-t", "emerald-thms-2-ad", "emerald-thms-2-an", "emerald-thms-2-ax"],
+				[thms]: [
+					[thms + "-1"],
+					[thms + "-1-01", thms + "-1-02", thms + "-1-03", thms + "-1-04"],
+					[thms + "-1-05", thms + "-1-06", thms + "-1-07", thms + "-1-08"],
+					[thms + "-2"],
+					[thms + "-2-01", thms + "-2-02", thms + "-2-03", thms + "-2-04", thms + "-2-05"],
+					[thms + "-2-06", thms + "-2-07", thms + "-2-08", thms + "-2-09", thms + "-2-10"],
+					[thms + "-2-11", thms + "-2-12", thms + "-2-13", thms + "-2-14", thms + "-2-15"],
+					[thms + "-2-16", thms + "-2-17", thms + "-2-18", thms + "-2-19", thms + "-2-20"],
+					[thms + "-2-21", thms + "-2-22", thms + "-2-23", thms + "-2-24", thms + "-2-25"],
+					[thms + "-2-26", thms + "-2-27", thms + "-2-28", thms + "-2-29", thms + "-2-30"],
+					[thms + "-2-31", thms + "-2-32", thms + "-2-33", thms + "-2-34", thms + "-2-35"],
+					[thms + "-2-36", thms + "-2-37", thms + "-2-38", thms + "-2-39", thms + "-2-40"],
+					[thms + "-2-41", thms + "-2-42", thms + "-2-43", thms + "-2-44", thms + "-2-45"],
+					[thms + "-2-46", thms + "-2-47", thms + "-2-48", thms + "-2-49", thms + "-2-50"],
 				],
-				"emerald-extra-credit": [
-					["emerald-extra-credit-1", "emerald-extra-credit-2"],
+				[extraCredit]: [
+					[extraCredit + "-1", extraCredit + "-2"]
 				],
 			},
 		},
