f1d6b9361c5648a6771eb48f2ade67c6aef1a666
JacobDGrieco <jacobdgrieco@gmail.com>
2025-12-09 22:43:08 -0500

    added TMs to Mega Dimensions

--- frontend/data/layouts/legendszamd.js
+++ frontend/data/layouts/legendszamd.js
@@ -36,7 +36,20 @@ PPGC.register({
 					[megaStones + "-2-06", megaStones + "-2-07", megaStones + "-2-08", megaStones + "-2-09", megaStones + "-2-10"],
 					[megaStones + "-2-11", megaStones + "-2-12", megaStones + "-2-13", megaStones + "-2-14", megaStones + "-2-15"],
 					[megaStones + "-2-16", megaStones + "-2-17", megaStones + "-2-18"],
-				]
+				],
+				[thms]: [
+					[thms + "-01", thms + "-02", thms + "-03", thms + "-04", thms + "-05"],
+					[thms + "-06", thms + "-07", thms + "-08", thms + "-09", thms + "-10"],
+					[thms + "-11", thms + "-12", thms + "-13", thms + "-14", thms + "-15"],
+					[thms + "-16", thms + "-17", thms + "-18", thms + "-19", thms + "-20"],
+					[thms + "-21", thms + "-22", thms + "-23", thms + "-24", thms + "-25"],
+					[thms + "-26", thms + "-27", thms + "-28", thms + "-29", thms + "-30"],
+					[thms + "-31", thms + "-32", thms + "-33", thms + "-34", thms + "-35"],
+					[thms + "-36", thms + "-37", thms + "-38", thms + "-39", thms + "-40"],
+					[thms + "-41", thms + "-42", thms + "-43", thms + "-44", thms + "-45"],
+					[thms + "-46", thms + "-47", thms + "-48", thms + "-49", thms + "-50"],
+					[thms + "-51", thms + "-52", thms + "-53"],
+				],
 			},
 		},
 		compact: {
